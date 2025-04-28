035ddc0340268fb0e87416be5ef00908fd494eea
josealbertoruizr <josealbertoruizr@gmail.com>
2025-04-28 12:35:29 -0600

    new updates

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
-import { loginRoute } from "@/routes/authRoutes"; // 👈 Correcto, llamamos a la RUTA
+import { loginRoute } from "@/routes/authRoutes";
 
 export default function LoginPage() {
   const router = useRouter();
